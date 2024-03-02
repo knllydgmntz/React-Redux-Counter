620cb4d6b162dea18666cb7e092e4c7a8b999ccc
knllydgmntz <knllydgmntz@gmail.com>
2024-03-02 21:29:10 +0800

    react redux counter

---
+++ src/store/index.js
@@ -0,0 +1,41 @@
+import { legacy_createStore as createStore } from "redux";
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    // dont use like this sample below!!
+    // state.counter++
+    // return state;
+    return {
+      counter: state.counter + 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
